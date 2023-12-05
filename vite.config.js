c15099acb6024dc81e7f4f627a042492f67630d0
模型 <1350323040@qq.com>
2023-12-05 18:07:01 +0800

    代码提交4

--- vite.config.js
+++ vite.config.js
@@ -29,15 +29,15 @@ export default defineConfig(({ mode }) => {
           chunkFileNames: 'static/js/[name]-[hash].js',
           entryFileNames: 'static/js/[name]-[hash].js',
           assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-          // manualChunks(id) {
-          //   if (id.includes("node_modules")) {
-          //     return id
-          //       .toString()
-          //       .split("node_modules/")[1]
-          //       .split("/")[0]
-          //       .toString();
-          //   }
-          // }
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+          }
         },
       },
     },
